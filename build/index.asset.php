445d3a067a49d9a8e933a81bf58b4961bd509303
Laurence Bahiirwa <laurence@freemius.com>
2023-04-11 08:29:19 +0300

    Fix the display name in block.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7cf01106aab595b248d4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7df0124da5a61fb87dbc');
